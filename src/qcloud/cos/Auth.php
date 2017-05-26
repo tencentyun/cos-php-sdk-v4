bf2fc78043e2b5ffe5bd55d218be0bd04f6ec796
ACTom <i@actom.me>
2017-05-26 22:23:51 +0800

    增加composer支持，支持操作多个API KEY

--- src/qcloud/cos/Auth.php
+++ src/qcloud/cos/Auth.php
@@ -3,7 +3,7 @@
  * Signature create related functions for authenticating with cos system.
  */
 
-namespace qcloudcos;
+namespace QCloud\Cos;
 
 /**
  * Auth class for creating reusable or nonreusable signature.
@@ -11,6 +11,15 @@ namespace qcloudcos;
 class Auth {
     // Secret id or secret key is not valid.
     const AUTH_SECRET_ID_KEY_ERROR = -1;
+    private $appId;
+    private $secretId;
+    private $secretKey;
+
+    public function __construct($appId, $secretId, $secretKey) {
+        $this->appId = $appId;
+        $this->secretId = $secretId;
+        $this->secretKey = $secretKey;
+    }
 
     /**
      * Create reusable signature for listDirectory in $bucket or uploadFile into $bucket.
@@ -19,23 +28,23 @@ class Auth {
      * Return the signature on success.
      * Return error code if parameter is not valid.
      */
-    public static function createReusableSignature($expiration, $bucket, $filepath = null) {
-        $appId = Conf::APP_ID;
-        $secretId = Conf::SECRET_ID;
-        $secretKey = Conf::SECRET_KEY;
+    public function createReusableSignature($expiration, $bucket, $filepath = null) {
+        $appId = $this->appId;
+        $secretId = $this->secretId;
+        $secretKey = $this->secretKey;
 
         if (empty($appId) || empty($secretId) || empty($secretKey)) {
             return self::AUTH_SECRET_ID_KEY_ERROR;
         }
 
         if (empty($filepath)) {
-            return self::createSignature($appId, $secretId, $secretKey, $expiration, $bucket, null);
+            return $this->createSignature($appId, $secretId, $secretKey, $expiration, $bucket, null);
         } else {
             if (preg_match('/^\//', $filepath) == 0) {
                 $filepath = '/' . $filepath;
             }
 
-            return self::createSignature($appId, $secretId, $secretKey, $expiration, $bucket, $filepath);
+            return $this->createSignature($appId, $secretId, $secretKey, $expiration, $bucket, $filepath);
         }
     }
 
@@ -45,10 +54,10 @@ class Auth {
      * Return the signature on success.
      * Return error code if parameter is not valid.
      */
-    public static function createNonreusableSignature($bucket, $filepath) {
-        $appId = Conf::APP_ID;
-        $secretId = Conf::SECRET_ID;
-        $secretKey = Conf::SECRET_KEY;
+    public function createNonreusableSignature($bucket, $filepath) {
+        $appId = $this->appId;
+        $secretId = $this->secretId;
+        $secretKey = $this->secretKey;
 
         if (empty($appId) || empty($secretId) || empty($secretKey)) {
             return self::AUTH_SECRET_ID_KEY_ERROR;
@@ -59,7 +68,7 @@ class Auth {
         }
         $fileId = '/' . $appId . '/' . $bucket . $filepath;
 
-        return self::createSignature($appId, $secretId, $secretKey, 0, $bucket, $fileId);
+        return $this->createSignature($appId, $secretId, $secretKey, 0, $bucket, $fileId);
     }
 
     /**
@@ -67,7 +76,7 @@ class Auth {
      * Return the signature on success.
      * Return error code if parameter is not valid.
      */
-    private static function createSignature(
+    private function createSignature(
             $appId, $secretId, $secretKey, $expiration, $bucket, $fileId) {
         if (empty($secretId) || empty($secretKey)) {
             return self::AUTH_SECRET_ID_KEY_ERROR;
